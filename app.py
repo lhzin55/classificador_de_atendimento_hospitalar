b2fb0a3c1819d7a160733838d833eb4fad1ae449
lhzin55 <eduardo.l.s.almeida14@gmail.com>
2026-08-01 15:16:30 -0300

    Alterando navamento o modelo, agora utilizando um ja validado

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def processar_triagem():
         # ----------------------------------------------------------------------
 
         payload = {
-            "model": "google/gemma-4-31b-it:free",
+            "model": "nvidia/nemotron-3.5-content-safety:free",
             "messages": [
                 {
                     "role": "system",
